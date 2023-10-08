d1a1b057d6471c0c802ab4a286ec3b50bf432399
Max mchantel <mikaerm65@gmail.com>
2023-10-08 03:55:20 +0300

    TEST-2 add tests

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ lazy val root = (project in file("."))
   .settings(
     name := "de-sber-test-project"
   )
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.15" % Test
---
+++ src/test/scala/task2/PositiveNegativeCountTest.scala
@@ -0,0 +1,22 @@
+package task2
+
+class PositiveNegativeCountTest extends org.scalatest.funsuite.AnyFunSuiteLike {
+
+  test("common cases") {
+    assert(PositiveNegativeCount.count(List(0, 0, 1, 0, 0, 1, 0, 1)) == 0)
+    assert(PositiveNegativeCount.count(List(1, 0, -1)) == 1)
+    assert(PositiveNegativeCount.count(List(1, 0, -1, 0, 0, 0, -1)) == 1)
+    assert(PositiveNegativeCount.count(List(1, -2, 3, 3, 4, -2, -2, -3, 1)) == 4)
+  }
+
+  test("corner cases") {
+    assert(PositiveNegativeCount.count(List()) == 0)
+    assert(PositiveNegativeCount.count(List(0)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, 0)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, 0, 1)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, 0, -1)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, 0, 1, 0)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, 0, -1, 0)) == 0)
+  }
+
+}
