b6c3c2b7366ce18686a81decf508a56321b178d1
Max mchantel <mikaerm65@gmail.com>
2023-10-08 04:20:16 +0300

    TEST-2 append corner cases test

--- src/test/scala/task2/PositiveNegativeCountTest.scala
+++ src/test/scala/task2/PositiveNegativeCountTest.scala
@@ -12,11 +12,23 @@ class PositiveNegativeCountTest extends org.scalatest.funsuite.AnyFunSuiteLike {
   test("corner cases") {
     assert(PositiveNegativeCount.count(List()) == 0)
     assert(PositiveNegativeCount.count(List(0)) == 0)
-    assert(PositiveNegativeCount.count(List(0, 0, 0, 0)) == 0)
-    assert(PositiveNegativeCount.count(List(0, 0, 0, 0, 1)) == 0)
-    assert(PositiveNegativeCount.count(List(0, 0, 0, 0, -1)) == 0)
-    assert(PositiveNegativeCount.count(List(0, 0, 0, 0, 1, 0)) == 0)
-    assert(PositiveNegativeCount.count(List(0, 0, 0, 0, -1, 0)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, 1)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, -1)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, 1, 0)) == 0)
+    assert(PositiveNegativeCount.count(List(0, 0, 0, -1, 0)) == 0)
+    assert(PositiveNegativeCount.count(List(1, 0, 0, 0)) == 0)
+    assert(PositiveNegativeCount.count(List(-1, 0, 0, 0)) == 0)
+    assert(PositiveNegativeCount.count(List(1, 0, 0, 0, -1)) == 1)
+    assert(PositiveNegativeCount.count(List(-1, 0, 0, 0, 1)) == 1)
+    assert(PositiveNegativeCount.count(List(Int.MaxValue, 1)) == 0)
+    assert(PositiveNegativeCount.count(List(Int.MaxValue, -1)) == 1)
+    assert(PositiveNegativeCount.count(List(Int.MinValue, 1)) == 1)
+    assert(PositiveNegativeCount.count(List(Int.MinValue, -1)) == 0)
+    assert(PositiveNegativeCount.count(List(1, Int.MaxValue)) == 0)
+    assert(PositiveNegativeCount.count(List(-1, Int.MaxValue)) == 1)
+    assert(PositiveNegativeCount.count(List(1, Int.MinValue)) == 1)
+    assert(PositiveNegativeCount.count(List(-1, Int.MinValue)) == 0)
   }
 
 }
